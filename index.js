4de3cfcd42afa183644e8ef765e26ed07dc227ce
vidhikotak15 <vidhi.kce21@sot.pdpu.ac.in>
2024-06-09 20:24:10 +0530

    nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("hello world, hi")
 })
 
 app.get('/twitter', (req, res) => {
